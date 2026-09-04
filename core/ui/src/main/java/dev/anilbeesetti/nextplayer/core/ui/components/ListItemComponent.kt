f4fae4db81cacf96854d1a7b4758d5d1cb7ef63b
Shadow <153516052+ShadowUR0@users.noreply.github.com>
2026-09-05 00:51:32 +0300

    Fix liquid list weight resolution

--- core/ui/src/main/java/dev/anilbeesetti/nextplayer/core/ui/components/ListItemComponent.kt
+++ core/ui/src/main/java/dev/anilbeesetti/nextplayer/core/ui/components/ListItemComponent.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
