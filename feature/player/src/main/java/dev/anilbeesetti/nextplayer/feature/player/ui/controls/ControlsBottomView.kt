ed77cd1bf9b9f22abe25f886e2b5f8ef7e27f4ad
Shadow <153516052+ShadowUR0@users.noreply.github.com>
2026-09-05 02:08:09 +0300

    Fix player seekbar syntax

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/ui/controls/ControlsBottomView.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/ui/controls/ControlsBottomView.kt
@@ -403,6 +403,7 @@ private fun SimpleSlider(
                         } else {
                             Modifier
                         },
+                    ),
             )
         },
         track = {
