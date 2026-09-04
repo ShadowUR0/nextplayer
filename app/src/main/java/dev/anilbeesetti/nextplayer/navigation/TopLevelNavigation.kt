6897753fd67a8b054be47656ffa0fb0be80ab98b
Shadow <153516052+ShadowUR0@users.noreply.github.com>
2026-09-04 19:41:30 +0300

    Fix navigation state delegate imports

--- app/src/main/java/dev/anilbeesetti/nextplayer/navigation/TopLevelNavigation.kt
+++ app/src/main/java/dev/anilbeesetti/nextplayer/navigation/TopLevelNavigation.kt
@@ -21,8 +21,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
@@ -75,7 +78,7 @@ fun rememberTopLevelNavState(): TopLevelNavState {
         backStack.ensureRoot(dest.route)
         dest.route to backStack
     }
-    val selectedIndex = rememberSaveable { androidx.compose.runtime.mutableIntStateOf(0) }
+    val selectedIndex = rememberSaveable { mutableIntStateOf(0) }
     return remember(backStacks, selectedIndex) {
         TopLevelNavState(destinations, backStacks, selectedIndex)
     }
